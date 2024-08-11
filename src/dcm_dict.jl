7cbc968b52727ea607f78cb412aad00b8aa834fd
curtcorum <1796102+curtcorum@users.noreply.github.com>
2024-08-11 15:50:59 -0500

    In progress enabling external dictionary file. modified:   dicom_dict_sandbox.jl modified:   src/dcm_dict.jl

--- src/dcm_dict.jl
+++ src/dcm_dict.jl
@@ -1,4 +1,4 @@
-dcm_dict = Dict(
+dcm_dict_default = Dict(
     (0x0002, 0x0000) => [:FileMetaInformationGroupLength, "UL", "1"],
     (0x0002, 0x0001) => [:FileMetaInformationVersion, "OB", "1"],
     (0x0002, 0x0002) => [:MediaStorageSOPClassUID, "UI", "1"],
@@ -5015,3 +5015,5 @@ dcm_dict = Dict(
     (0xfffe, 0xe00d) => [:ItemDelimitationItem, "See Note", "1"],
     (0xfffe, 0xe0dd) => [:SequenceDelimitationItem, "See Note", "1"],
 )
+
+dcm_dict = dicom_dict_default
