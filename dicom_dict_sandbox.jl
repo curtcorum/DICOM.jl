9ecd6c3352a1ec5d120e3f611f0bbd0b7e46e698
curtcorum <1796102+curtcorum@users.noreply.github.com>
2024-08-11 16:47:45 -0500

    Comparison of dicts. modified:   dicom_dict_sandbox.jl

--- dicom_dict_sandbox.jl
+++ dicom_dict_sandbox.jl
@@ -49,21 +49,27 @@ now()
 my_dict_file = "/home/curt/src/DICOM.jl/dicts/gems/gems-dicom-dict.txt"
 
 # ╔═╡ b8f72b73-a0ec-42b0-99af-d02d5b8cd2fb
-my_raw_dict = readdlm(my_dict_file, '\t', String, '\n'; header=false, comments=true, comment_char='#')
+my_raw_dict = readdlm(my_dict_file, '\t', String, '\n'; header=false, comments=true, comment_char='#');
 
 # ╔═╡ 1cffb689-518a-4ac1-8c89-aa6d14f70916
 # Reformat into DICOM.dcm_dict format
 begin
 	my_dcm_dict = Dict{Tuple{UInt16, UInt16}, Vector{Any}}()
 	for row in axes( my_raw_dict, 1)
-		#println( row, " ", col, " ", my_raw_dict[row, col])
 		# ignore or skip "xx" for now?
-		dcm_tag = parse( UInt32, replace( my_raw_dict[row, 1], '(' => "", ')' => "", ',' => "", 'x' => "f"), base=16)
+		dcm_tag_raw = string( parse( UInt32, replace( my_raw_dict[row, 1], '(' => "", ')' => "", ',' => "", 'x' => "f"), base=16), base=16, pad=8)
+		bytes_array = hex2bytes( dcm_tag_raw)
 		
-		#dcm_tag_l = parse( UInt16, string( tag_low), base=16)
-		#dcm_tag_tmp = (dcm_tag_h, dcm_tag_l)
-		#my_dcm_dict[dcm_tag_tmp] = [Symbol( name), vr, vm]
-		println( row, "\t", string( dcm_tag, base=16, pad=8), "\t", my_raw_dict[row, 3])
+		dcm_tag_l = parse( UInt16, bytes2hex( bytes_array[3:4]), base=16)
+		dcm_tag_h = parse( UInt16, bytes2hex( bytes_array[1:2]), base=16)
+		dcm_tag = (dcm_tag_h, dcm_tag_l)
+		
+		vr =  my_raw_dict[row, 2]
+		name =  my_raw_dict[row, 3]
+		vm =  my_raw_dict[row, 4]
+
+		println( row, "\t", dcm_tag, "\t", vr, "\t", name, "\t", vm )
+		my_dcm_dict[dcm_tag] = [Symbol( name), vr, vm]
 	end
 end
 
@@ -77,68 +83,30 @@ my_dcm_dict
 #This is the default dictionary for DICOM.jl
 dcm_dict_default = DICOM.dcm_dict
 
-# ╔═╡ 437ede2c-c649-48fd-af98-346f46468204
-# ╠═╡ show_logs = false
-# ╠═╡ disabled = true
-#=╠═╡
-# loop through dcm_dict_default and compare to my_dcm_dict
+# ╔═╡ 4c827956-4632-4982-8356-409417bcd712
 for (key, value) in dcm_dict_default
+	#println( key)
 	def_name = value[1]
 	if haskey( my_dcm_dict, key)
-		my_name = (my_dcm_dict[key])[1]
-		if def_name == my_name
-			# nothing
-		else
-			println( key, " NOT EQUAL ", def_name, " ", my_name)
-		end
+		#println( key, " PRESENT ", def_name)
 	else
 		println( key, " MISSING ", def_name)
 	end
 end
-  ╠═╡ =#
 
-# ╔═╡ e418bf76-8bfc-499c-b77a-05c96619a347
-# ╠═╡ show_logs = false
-# ╠═╡ disabled = true
-#=╠═╡
-# loop through  my_dcm_dict and compare to dcm_dict_default
+# ╔═╡ 2a985def-3477-434d-96b8-3097564ccd91
 for (key, value) in my_dcm_dict
-	my_name = value[1]
+	#println( key)
+	def_name = value[1]
 	if haskey( dcm_dict_default, key)
-		def_name = (dcm_dict_default[key])[1]
-		if def_name == my_name
-			# nothing
-		else
-			println( key, " NOT EQUAL ", my_name, " ", def_name)
-		end
+		#println( key, " PRESENT ", def_name)
 	else
-		println( key, " MISSING ", my_name)
+		println( key, " MISSING ", def_name)
 	end
 end
-  ╠═╡ =#
-
-# ╔═╡ 0fb7ed85-a4f0-481d-bb95-591a4b36ec9a
-# ╠═╡ disabled = true
-#=╠═╡
-dcm_dict_plus_my = copy( dcm_dict_default);
-  ╠═╡ =#
 
 # ╔═╡ 973c001d-a94b-4085-a4f8-3c7c2549d02b
 vr_plus_my = Dict( (0x0000, 0x0000) => "")
-
-# ╔═╡ 3ded8e96-8193-4f1b-bd93-bdccb18c07bb
-# ╠═╡ disabled = true
-#=╠═╡
-# loop through  my_dcm_dict and append to dcm_dict_default if key missing
-for (key, value) in my_dcm_dict
-	if haskey( dcm_dict_default, key)
-		# nothing
-	else
-		vr_plus_my[key] = (my_dcm_dict[key])[2]
-		dcm_dict_plus_my[key] =  (my_dcm_dict[key])[1:3]
-	end
-end
-  ╠═╡ =#
 
 # ╔═╡ 66b43451-4382-4aa8-8291-b9e8104514f3
 # ╠═╡ disabled = true
@@ -177,11 +145,9 @@ simshow( dcm_data_test.PixelData; γ=2)
 # ╠═74c5d6fd-3206-4901-b5ea-3e752fe84418
 # ╠═e22fbfcb-5f39-4e9a-8aed-a6f1b87b4f27
 # ╠═a407726f-b5e7-4672-bf91-732c75ae4392
-# ╠═437ede2c-c649-48fd-af98-346f46468204
-# ╠═e418bf76-8bfc-499c-b77a-05c96619a347
-# ╠═0fb7ed85-a4f0-481d-bb95-591a4b36ec9a
+# ╠═4c827956-4632-4982-8356-409417bcd712
+# ╠═2a985def-3477-434d-96b8-3097564ccd91
 # ╠═973c001d-a94b-4085-a4f8-3c7c2549d02b
-# ╠═3ded8e96-8193-4f1b-bd93-bdccb18c07bb
 # ╠═66b43451-4382-4aa8-8291-b9e8104514f3
 # ╠═5cca6bcb-0333-4fca-89bd-4bcc7921cd79
 # ╠═9b0336e1-83b7-43d8-bcde-a9ada0f7e964
